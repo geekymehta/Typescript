bc1a7aa3bf223dbce34423dd4c90628e6c12ba03
sumit514 <sumitmehta514@gmail.com>
2024-01-27 14:47:39 +0530

    first commit

---
+++ intro/intro.js
@@ -0,0 +1,3 @@
+var user = { name: 'Jack', age: 32, location: 'USA' };
+console.log('Hello World');
+console.log(user.name);
---
+++ intro/intro.ts
@@ -0,0 +1,4 @@
+let user = {name: 'Jack', age: 32, location: 'USA'};
+
+console.log('Hello World');
+console.log(user.name);
